02d8593588702927841d8ac0e0bb8d9c7fef2d7d
andriali044 <andriali044gmail.com>
2022-09-09 15:05:54 +0700

    first backend]

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+const usersmodel = require('../models/usersmodel')
+const bcrypt = require("bcryptjs")
+const {validation} = require('../middleware/validation')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const login = async(req, res) => {
+    try {
+        const username = req.body.username
+        const password = req.body.password
+        const getdata = await usersmodel.findOne({
+            where: {username: username}
+        })
+
+        if(!getdata) res.status(400).send("username tidak terdaftar")
+        const resultLogin = bcrypt.compareSync(password, getdata.password)
+
+        if(!resultLogin) res.status(400).send("username atau password tidak sama")
+
+        //token
+        const token = jwt.sign({_username: getdata.username}, process.env.TOKEN_RAHASIA)
+        res.header('auth-token', token).send('berhasil login')
+        
+        
+    } catch (error) {
+        res.status(400).send("Erorr bos")
+    }
+}
+const register = async(req,res) =>{
+    try {
+        const username = req.body.username
+        const password = req.body.password
+        const email = req.body.email
+
+        //validasi form
+        
+
+        const{error} = validation(req.body)
+
+        if(error) return res.status(400).send(error.details[0].message)
+        
+        //jika sudah sesuai kriteria simpan data
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = await bcrypt.hashSync(password, salt)
+
+
+        const users = new usersmodel({
+            username : username,
+            password : hashedPassword,
+            email : email,
+        })
+
+        const savedUser = await users.save();
+        res.json(savedUser)
+    } catch (error) {
+        res.status(400).send("error bos")
+    }
+}
+
+module.exports = {
+    register, login
+}
--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router();
 const sampleController = require('../controllers/sampleController');
 const foodController = require('../controllers/foodscontroller');
-// const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 const foodsmodel = require('../models/foodsModel');
+const verifitoken = require('../middleware/verifytoken')
 
 router.get('/', sampleController.methodGet);
 router.post('/', sampleController.methodPost);
@@ -12,7 +13,7 @@ router.delete('/', sampleController.methodDelete);
 
 //foods
 router.post('/foods', foodController.methodPost);
-router.get('/foods', foodController.methodGet);
+router.get('/foods', verifitoken, foodController.methodGet);
 router.get('/foods/:id', foodController.methodGetId);
 router.put('/foods/:id', foodController.methodPut);
 router.delete('/foods/:id', foodController.methodDelete);
@@ -20,5 +21,6 @@ router.delete('/foods/:id', foodController.methodDelete);
 router.post('/foods/upload', foodController.methodUploadFoods);
 router.post('/foods/search', foodController.methodGetCondition);
 
-// router.post('/auth/register',authController.register)
+router.post('/auth/register', authController.register)
+router.post('/auth/login', authController.login)
 module.exports = router
